9ffd0a6f5d8de5a242074953dc750328aaba58b5
Riva Quiroga <riva.quiroga@uc.cl>
2020-06-15 18:06:42 -0400

    update guaguas dataset

--- R/guaguas.R
+++ R/guaguas.R
@@ -6,12 +6,13 @@ NULL
 #' entre 1920 y 2019, según el Servicio de Registro Civil
 #' e Identificación. Incluye todos los nombres con al
 #' menos 15 ocurrencias.
-#' @format Un dataframe con 84565 filas y 4 variables
+#' @format Un dataframe con 84565 filas y 5 variables
 #' \describe{
-#'   \item{anio}{Año}
+#'   \item{anio}{Año de inscripción}
 #'   \item{sexo}{Sexo registral}
 #'   \item{nombre}{Nombre}
 #'   \item{n}{Frecuencia del nombre ese año}
+#'   \item{proporcion}{\code{n} dividido por el total de inscripciones de ese año}
 #' }
 #' @source Servicio de Registro Civil e Identificación
 #'     de Chile
--- data-raw/guaguas.R
+++ data-raw/guaguas.R
@@ -3,11 +3,22 @@
 library(readxl)
 library(dplyr)
 library(stringr)
+library(lubridate)
 
-guaguas <- read_xlsx("data-raw/nombres_registro_civil.xlsx") %>%
+nombres <- read_xlsx("data-raw/nombres_registro_civil.xlsx") %>%
   rename(anio = AÑO, sexo = SEXO, nombre = NOMBRE, n = CANTIDAD) %>%
   mutate(nombre = str_to_title(nombre)) %>%
   filter(anio != 2020) %>%
   arrange(anio, desc(n))
+
+nacimientos <- read_xlsx("data-raw/nacimientos_registro_civil.xlsx") %>%
+  select(anio = `AÑO INSC NAC`, tiporegistro = `REGISTRO NAC`, cantidad) %>%
+  group_by(anio) %>%
+  summarise(inscritos = sum(cantidad))
+
+guaguas <- left_join(nombres, nacimientos) %>%
+  mutate(n = as.integer(n)) %>%
+  mutate(proporcion = zapsmall(n / inscritos)) %>%
+  select(-inscritos)
 
 usethis::use_data(guaguas, overwrite = TRUE)
