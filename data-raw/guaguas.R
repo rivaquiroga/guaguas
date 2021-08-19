5095e6886fd7e38318a403bf9d62800d8a481f81
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-19 12:34:44 +0800

    Delete data-raw directory

--- data-raw/guaguas.R
+++
@@ -1,9 +0,0 @@
-
-guaguas <- readr::read_csv("data-raw/1920-2020.csv")
-
-
-
-guaguas_frecuentes <- guaguas %>%
-  dplyr::filter(n >= 15)
-
-usethis::use_data(guaguas, guaguas_frecuentes, internal = TRUE, overwrite = TRUE)
